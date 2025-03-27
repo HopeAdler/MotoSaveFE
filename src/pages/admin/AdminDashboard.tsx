1ddec7da174ce522b6efd7782af0a05a05b39c2f
Nadariano <aduy1912@gmail.com>
2025-03-27 21:42:16 +0700

    add vercel.json

---
+++ src/pages/admin/AdminDashboard.tsx
@@ -0,0 +1,13 @@
+import { Card, Typography } from "antd";
+
+const AdminDashboard = () => {
+
+  return (
+      <Card className="p-6 shadow-lg">
+        <Typography className="text-2xl font-bold mb-4">Admin Dashboard</Typography>
+        <Typography className="text-lg">Welcome, Admin!</Typography>
+      </Card>
+  )
+};
+
+export default AdminDashboard;
